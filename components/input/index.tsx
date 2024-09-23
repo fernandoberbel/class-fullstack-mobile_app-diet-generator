06b6e1631c9c422128bb71dcdaa9d82afb5978d1
fernandoberbel <fberbel1000@gmail.com>
2024-09-23 20:10:29 -0300

    feat: created input component

---
+++ components/input/index.tsx
@@ -0,0 +1,65 @@
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  KeyboardTypeOptions,
+  Text,
+} from "react-native";
+import { colors } from "../../constants/colors";
+import { Controller } from "react-hook-form";
+
+interface InputProps {
+  name: string;
+  control: any;
+  placeholder?: string;
+  rules?: object;
+  error?: string;
+  keyboardType: KeyboardTypeOptions;
+}
+
+export function Input({
+  name,
+  control,
+  placeholder,
+  rules,
+  error,
+  keyboardType,
+}: InputProps) {
+  return (
+    <View style={styles.container}>
+      <Controller
+        control={control}
+        name={name}
+        rules={rules}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder={placeholder}
+            onBlur={onBlur}
+            value={value}
+            onChangeText={onChange}
+            keyboardType={keyboardType}
+          />
+        )}
+      />
+
+      {error && <Text style={styles.errorText}>{error}</Text>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 16,
+  },
+  input: {
+    height: 44,
+    backgroundColor: colors.white,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+  },
+  errorText: {
+    color: "red",
+    marginTop: 4,
+  },
+});
